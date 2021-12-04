cc50dc3fbf802176f9d24e525427bd97a9b5b634
Yonanni <adhanamengisteab@gmail.com>
2021-12-04 23:06:40 +0100

    delete not working

--- src/components/blog/blog-item/index.jsx
+++ src/components/blog/blog-item/index.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import BlogAuthor from "../blog-author";
 import { Link } from "react-router-dom";
 import "./styles.css";
-export default class BlogItem extends Component {
-  render() {
-    const { title, cover, author, _id } = this.props;
+// export default class BlogItem extends Component {
+const BlogItem = (props) => { 
+
+  const deleteBlog = async (blogID) => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/blogPosts/${blogID}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+          },
+          method: "DELETE",
+        }
+      );
+      if (response.ok) {
+        alert("Delete");
+        // const resp = await response.json()
+      } else {
+        alert("somethin wrong");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+  // render() {
+    // const { title, cover, author, _id } = this.props;
+    const { title, cover, author, _id } = props;
     return (
+      <>
       <Link to={`/blog/${_id}`} className="blog-link">
         <Card className="blog-card">
           <Card.Img variant="top" src={cover} className="blog-cover" />
@@ -18,6 +46,9 @@ export default class BlogItem extends Component {
           </Card.Footer>
         </Card>
       </Link>
+      <Button className="delete" onClick={()=> deleteBlog(_id)}>Delete</Button>
+      </>
     );
-  }
+  // }
 }
+export default BlogItem
